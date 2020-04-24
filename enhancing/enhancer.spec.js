540654b862b674eb37a15620183d5fdf3efead61
Alec Dye <alecdye@gmail.com>
2020-04-23 19:19:15 -0700

    Completing all required tests

--- enhancing/enhancer.spec.js
+++ enhancing/enhancer.spec.js
@@ -13,8 +13,35 @@ describe("enhancer.js", () => {
     it("should return true if enchanement succeeded", function () {
       expect(succeed(testItem).enhancement).toBe(11);
     });
+    it("should not increase enhancement past lvl 20", function () {
+      const newTestItem = {
+        enhancement: 20,
+      };
+      expect(succeed(newTestItem).enhancement).toBe(20);
+    });
   });
-  describe(".fail()", function () {});
+
+  describe(".fail()", function () {
+    it("should decrease durability by 5", function () {
+      expect(fail(testItem).durability).toBe(45);
+    });
+    it("enhancement is 16 or greater, durability decreases by 10 and enhancement decreases by 1", function () {
+      const newTestItem = {
+        enhancement: 16,
+        durability: 13,
+      };
+      expect(fail(newTestItem).durability).toBe(3);
+      expect(fail(newTestItem).enhancement).toBe(16);
+    });
+    it("enhancement is 15, durablitiy should decrease by 10", function () {
+      const newTestItem = {
+        enhancement: 15,
+        durability: 2,
+      };
+      expect(fail(newTestItem).durability).toBe(-8);
+    });
+  });
+
   describe(".repair()", function () {
     it("should increase durability to 100", function () {
       expect(repair(testItem).durability).toBe(100);
