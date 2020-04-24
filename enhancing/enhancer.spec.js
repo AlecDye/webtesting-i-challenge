0f9b0b711320c21a22cec4a4b2bf1617c9dea77b
Alec Dye <alecdye@gmail.com>
2020-04-23 18:38:48 -0700

    Building enhance methods and tests

--- enhancing/enhancer.spec.js
+++ enhancing/enhancer.spec.js
@@ -1,7 +1,23 @@
 const enhancer = require("./enhancer.js");
+const { succeed, fail, repair } = require("./enhancer");
+
+const testItem = {
+  name: "sword",
+  enhancement: 10,
+  durability: 50,
+};
+
 // test away!
 describe("enhancer.js", () => {
-  it("should run tests without errors", function () {
-    expect(true).toBe(true);
+  describe(".succeed()", function () {
+    it("should return true if enchanement succeeded", function () {
+      expect(succeed(testItem).enhancement).toBe(11);
+    });
+  });
+  describe(".fail()", function () {});
+  describe(".repair()", function () {
+    it("should increase durability to 100", function () {
+      expect(repair(testItem).durability).toBe(100);
+    });
   });
 });
